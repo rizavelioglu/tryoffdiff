9e54e5e370ce2f526b66e21232e887cdeaeafa28
rizavelioglu <rzavelioglu@gmail.com>
2024-11-25 19:20:41 +0100

    update pages

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project Webpage of TryOffDiff
+
+To build an existing page locally and to serve:
+
+```bash
+jekyll build
+jekyll serve --livereload
+```
+
+To build a fresh page:
+```bash
+jekyll new . --force
+bundle exec jekyll serve --livereload
+```
---
+++ static/js/index.js
@@ -0,0 +1,31 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+
+$(document).ready(function() {
+
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 10000,
+    }
+
+	// Initialize all div with teaser-carousel class
+    var carousels = bulmaCarousel.attach('.teaser-carousel', options);
+
+
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 10000,
+    }
+	// Initialize all div with scale-carousel class
+    var carousels = bulmaCarousel.attach('.scale-carousel', options);
+
+
+})
